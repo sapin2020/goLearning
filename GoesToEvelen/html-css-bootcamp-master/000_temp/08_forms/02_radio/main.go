1cf3abcadf35ed195037a806349986e9252360b4
sapin2020 <saxon_8421@yahoo.com>
2022-06-05 15:12:19 +0800

    commit all

---
+++ GoesToEvelen/html-css-bootcamp-master/000_temp/08_forms/02_radio/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/process", process)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+	i := r.FormValue("item")
+	fmt.Println(i)
+	tpl.ExecuteTemplate(w, "process.gohtml", i)
+}
